49986b1467e72698743032981c97132fb630a045
Frederick Lazo <fredlazo@yahoo.com>
2017-02-28 21:44:56 -0800

    Lesson 5 completed

--- src/app/index.js
+++ src/app/index.js
@@ -3,19 +3,32 @@ var ReactDOM = require('react-dom');
 
 //Create component
 var TodoComponent = React.createClass({
+  getInitialState: function(){
+    return {
+      todos: ['Eat', 'Sleep', 'Poop'],
+      food: ['Bacon', 'Donuts', 'Butter']
+    }
+  },
   render: function(){
     return (
       <div>
-        <h1>City: {this.props.myTeam.city}</h1>
-        <h1>Team: {this.props.myTeam.name}</h1>
-        <h1>Mascot: {this.props.myTeam.mascot}</h1>
-        <p>{this.props.message}</p>
+      <p>My To Do List</p>
+      <ul>
+        <li>{this.state.todos[0]}</li>
+        <li>{this.state.todos[1]}</li>
+        <li>{this.state.todos[2]}</li>
+      </ul>
+      <p>My To Eat List</p>
+      <ul>
+        <li>{this.state.food[0]}</li>
+        <li>{this.state.food[1]}</li>
+        <li>{this.state.food[2]}</li>
+      </ul>
+
       </div>
     );
   }
 });
 
-var team ={city:'San Francisco', name:'Giants', mascot:'Lou Seal'};
-
 //Put component into html page
-ReactDOM.render(<TodoComponent message="This is a prop" myTeam={team}/>, document.getElementById('todo-wrapper'));
+ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
