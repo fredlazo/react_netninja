d70c1284c0fb5e7d72a442b6b19a5821a1824035
Frederick Lazo <fredlazo@yahoo.com>
2017-02-28 21:23:19 -0800

    Lesson 4 completed

--- src/app/index.js
+++ src/app/index.js
@@ -6,12 +6,16 @@ var TodoComponent = React.createClass({
   render: function(){
     return (
       <div>
-        <h1>Crap-tastic!</h1>
-        <h2>Westside!</h2>
+        <h1>City: {this.props.myTeam.city}</h1>
+        <h1>Team: {this.props.myTeam.name}</h1>
+        <h1>Mascot: {this.props.myTeam.mascot}</h1>
+        <p>{this.props.message}</p>
       </div>
     );
   }
 });
 
+var team ={city:'San Francisco', name:'Giants', mascot:'Lou Seal'};
+
 //Put component into html page
-ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
+ReactDOM.render(<TodoComponent message="This is a prop" myTeam={team}/>, document.getElementById('todo-wrapper'));
