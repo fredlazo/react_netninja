f923b4908d993337e10b8fcedcd73a970e17dd22
Frederick Lazo <fredlazo@yahoo.com>
2017-03-02 22:47:11 -0800

    Lesson 8 - 2:45

--- src/app/index.js
+++ src/app/index.js
@@ -1,33 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-//Create component
-var TodoComponent = React.createClass({
-  getInitialState: function(){
-    return {
-      todos: ['Eat', 'Sleep', 'Poop'],
-    }
-  },
-  render: function(){
-    var todos = this.state.todos;
-    todos = todos.map(function(item, index){
-      return (
-          <TodoItem item={item} key={index}/>
-        );
-      });
 
-    return (
-      <div>
-        <p>My To Do List</p>
-        <ul>
-          {todos}
-        </ul>
-      </div>
-    );
-  }
-});
-
-//TodoItemComponent
+//TodoItem Component
 var TodoItem = React.createClass({
   render: function(){
     return(
@@ -39,6 +14,67 @@ var TodoItem = React.createClass({
     );
   }
 });
+
+/*
+Todo Component
+  1. State (Data)
+  2. Render (HTML)
+  3. Events
+*/
+var TodoComponent = React.createClass({
+  getInitialState: function(){
+    return {
+      todos: ['Eat', 'Sleep', 'Poop'],
+    }
+  },
+
+/**************************
+render:  function(){
+
+  var = function()[]
+      return (one array[index]) this.
+    };
+    return( insert this html into index.html);
+
+]
+*/
+
+
+  render: function(){
+    //The TodoItem component (above) only creates ONE <li>{item}</li>
+    //Therefore, we need to iterate through todos array with map, passing 'item' and 'index'
+    //into the TodoItem component each time, resulting in a new todos array of  [<li>{item}</li>] elements.
+
+    var todos = this.state.todos;
+    todos = todos.map(function(item, index){
+        return (
+            <TodoItem item={item} key={index}/> //Put component into html page
+          );
+      });
+
+      //END RESULT:  todos = [<li>Eat<</li>, <li>Sleep</li>, <li>Poop</li>]
+/***************************/
+    //THIS HTML IS CREATED.
+    return (
+      <div id="todo-list">
+        <p onClick={this.clicked}>My To Do List</p>
+          <ul>
+            {todos}
+          </ul>
+      </div>
+    );
+  }, //End of render function
+
+/***************************/
+
+  clicked: function(){
+    console.log('You clicked me!');
+  }
+
+});
+
+/************End of TodoComponent*****************/
+
 
 //Put component into html page
 ReactDOM.render(<TodoComponent></TodoComponent>, document.getElementById('todo-wrapper'));
