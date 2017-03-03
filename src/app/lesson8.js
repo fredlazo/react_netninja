00983804d056c5df065c3d3fb8362d8da12af2f9
Frederick Lazo <fredlazo@yahoo.com>
2017-03-03 11:19:01 -0800

    Lesson 8 completed

--- src/app/lesson8.js
+++ src/app/lesson8.js
@@ -1,6 +1,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+
+//TodoItem Component
+var TodoItem = React.createClass({
+  render: function(){
+    return(
+      <li>
+        <div className = 'todo-item'>
+          <span className = 'item-name'>{this.props.item}</span>
+        </div>
+      </li>
+    );
+  }
+});
+
 /*
 Todo Component
   1. State (Data)
@@ -10,7 +24,7 @@ Todo Component
 var TodoComponent = React.createClass({
   getInitialState: function(){
     return {
-      todos: ['Eat', 'Sleep', 'Poop', 'Pee', 'Fart', 'Code'],
+      todos: ['Eat', 'Sleep', 'Poop'],
     }
   },
 
@@ -24,24 +38,26 @@ render:  function(){
 
 ]
 */
+
+
   render: function(){
-    //The TodoItem component (below) only returns ONE <li>{item}</li>
+    //The TodoItem component (above) only creates ONE <li>{item}</li>
     //Therefore, we need to iterate through todos array with map, passing 'item' and 'index'
     //into the TodoItem component each time, resulting in a new todos array of  [<li>{item}</li>] elements.
 
     var todos = this.state.todos;
     todos = todos.map(function(item, index){
         return (
-            <TodoItem item={item} key={index} onDelete={this.onDelete}/> //Put component into html page
+            <TodoItem item={item} key={index}/> //Put component into html page
           );
-      }.bind(this));
+      });
 
       //END RESULT:  todos = [<li>Eat<</li>, <li>Sleep</li>, <li>Poop</li>]
 /***************************/
     //THIS HTML IS CREATED.
     return (
       <div id="todo-list">
-        <p>My To Do List</p>
+        <p onClick={this.clicked}>My To Do List</p>
           <ul>
             {todos}
           </ul>
@@ -49,40 +65,16 @@ render:  function(){
     );
   }, //End of render function
 
-  //Custom functions
-  onDelete: function(item){
-    var updatedTodos = this.state.todos.filter(function(val,index){
-      return item !== val;
-    });
-    this.setState({
-      todos: updatedTodos
-    });
+/***************************/
+
+  clicked: function(){
+    console.log('You clicked me!');
   }
+
 });
 
 /************End of TodoComponent*****************/
 
-//TodoItem Component
-// this. (TodoItem Component) has a props item={item} equal to
-//'Eat' or 'Sleep' etc.
-var TodoItem = React.createClass({
-  render: function(){
-    return(
-      <li>
-        <div className = 'todo-item'>
-          <span className = 'item-name'>{this.props.item}</span>
-          <span className = 'item-delete' onClick = {this.handleDelete}> x </span>
-        </div>
-      </li>
-    );
-  },
-
-//Custom functions
-  handleDelete: function(){
-    this.props.onDelete(this.props.item);
-  }
-
-});
 
 //Put component into html page
 ReactDOM.render(<TodoComponent></TodoComponent>, document.getElementById('todo-wrapper'));
